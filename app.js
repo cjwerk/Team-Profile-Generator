d70a230929daaa8df6829b68b3360fcca67e8ff1
Chris Werkheiser <cjwerk@gmail.com>
2020-11-26 18:29:59 -0800

    making progress

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const fs = require("fs");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+const { type } = require("os");
 
 const employees = [];
+
+function initApp() {
+    newEmployee();
+}
 
 function newEmployee() {
     inquirer.prompt([{
@@ -29,5 +34,48 @@ function newEmployee() {
         message: 'Please enter employees email',
         name: 'Email'
     },
-]);
+    ])
+        .then(function ({ name, role, id, email }) {
+            let roleInfo = "",
+            if (role === "Engineer") {
+                roleInfo = "Github username";
+            } else if (role === 'Intern') {
+                roleInfo = 'school name'
+            } else {
+                roleInfo = 'Office phone number';
+            }
+            inquirer.prompt({
+
+                message: `Please enter team members ${roleInfo}`,
+                name: 'roleInfo'
+            },
+                {
+                    type: 'list',
+                    message: 'Would you like to add more team members?',
+                    choices: [
+                        'yes',
+                        'no',
+                    ],
+                    name: 'moreMembers'
+                })
+                .then(function ({ roleInfo, moreMembers }) {
+                    let newMember;
+                    if (role === 'Engineer') {
+                        newMember = new Engineer(name, id, email, roleInfo);
+                    } else if (role === 'Intern') {
+                        newMember = new Intern(name, id, email, roleInfo);
+                    } else {
+                        newMember = new Manager(name, id, email, roleInfo);
+                    }
+                    employees.push(newMember);
+                    addHtml(newMember)
+                        .then(function () {
+                            if (moreMembers === 'yes') {
+                                newEmployee();
+                            } else {
+                                finishHtml();
+                            };
+                        });
+                });
+        });
 }
